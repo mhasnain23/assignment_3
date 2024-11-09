af09222f6a5a5af194b893c0ca4663321ba94020
Hasnain Siddiqui <146128813+mhasnain23@users.noreply.github.com>
2024-11-09 14:33:42 +0500

    Update card.tsx

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,6 +1,6 @@
 const Card = (props: any) => {
   return (
-    <div className="flex flex-col pl-5 items-start justify-center border-2 w-52 h-24">
+    <div className="flex flex-col pl-5 items-start justify-center border-2 w-52 h-24 -mt-[20px]">
       <h2>Name: {props.name}</h2>
       <h2>Id: {props.Id}</h2>
       <h2>Day: {props.day}</h2>
