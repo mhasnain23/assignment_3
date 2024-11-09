965c2d8a61d76c152ab80a80789fb060290ce3b1
Hasnain Siddiqui <146128813+mhasnain23@users.noreply.github.com>
2024-11-09 14:41:19 +0500

    Update card.tsx

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,11 +1,11 @@
-const Card = (props: any) => {
-  return (
-    <div className="flex flex-col pl-5 items-start justify-center border-2 w-52 h-24 -mt-[20px]">
-      <h2>Name: {props.name}</h2>
-      <h2>Id: {props.Id}</h2>
-      <h2>Day: {props.day}</h2>
-    </div>
-  );
-};
+//const Card = (props: any) => {
+  //return (
+    //<div className="flex flex-col pl-5 items-start justify-center border-2 w-52 h-24 -mt-[20px]">
+      //<h2>Name: {props.name}</h2>
+     // <h2>Id: {props.Id}</h2>
+      //<h2>Day: {props.day}</h2>
+    //</div>
+  //);
+//};
 
-export default Card;
+//export default Card;
